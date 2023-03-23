94b7dc336145640598f216a17bdf6556b63e53dc
Anupam Gautam <62124441+anupam-gautam@users.noreply.github.com>
2023-03-23 21:33:09 +0545

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,34 +1,17 @@
-import EchoApp.Echo;
-import EchoApp.EchoHelper;
-import org.omg.CORBA.ORB;
-import org.omg.CORBA.ORBPackage.InvalidName;
-import org.omg.CosNaming.NamingContextExt;
-import org.omg.CosNaming.NamingContextExtHelper;
-import org.omg.CosNaming.NamingContextPackage.CannotProceed;
-import org.omg.CosNaming.NamingContextPackage.NotFound;
+interface Template{
+    int add(int x,int y);
+}
 
-public class Client {
-
-    public static void main(String args[]) {
-        try {
-            // create and initialize the ORB
-            ORB orb = ORB.init(args, null);
-            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
-            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
-            Echo href = EchoHelper.narrow(ncRef.resolve_str("ECHO-SERVER"));
-
-            String hello = href.echoString();
-            System.out.println(hello);
-        } catch (InvalidName invalidName) {
-            invalidName.printStackTrace();
-        } catch (CannotProceed cannotProceed) {
-            cannotProceed.printStackTrace();
-        } catch (org.omg.CosNaming.NamingContextPackage.InvalidName invalidName) {
-            invalidName.printStackTrace();
-        } catch (NotFound notFound) {
-            notFound.printStackTrace();
-        }
-
+class TemplateBase extends Template{
+    public int add(int x, int y){
+        return x+y;
     }
+}
 
+public class Client{
+    ORB orb = ORB.init(args,null);
+    org.omg.CORBA.Object obj = orb.resolve_Initial_References("Name");
+    NamingContext ext = NamingContextHelper.narrow(obj);
+    TemplateBase temp = TemplateBase.narrow(ext.resolve_reference(ext));
+    ext.add(40,40);
 }
